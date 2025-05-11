46555b1dc85960b2260cf5dff682f7019c04063e
artemiyulyanov <artemiyulyanov.career@gmail.com>
2025-05-12 02:31:30 +0300

    docs: bearerAuth added in Swagger

--- src/main/java/me/artemiyulyanov/uptodate/configuration/SwaggerConfig.java
+++ src/main/java/me/artemiyulyanov/uptodate/configuration/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package me.artemiyulyanov.uptodate.configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +14,15 @@ import org.springframework.context.annotation.Configuration;
         info = @Info(title = "Uptodate API", version = "v1.0.0"),
         servers = @Server(url = "https://artemyulyanov.com", description = "Production server")
 )
+@SecuritySchemes({
+        @SecurityScheme(
+                name = "bearerAuth",
+                type = SecuritySchemeType.HTTP,
+                scheme = "bearer",
+                bearerFormat = "JWT",
+                in = SecuritySchemeIn.HEADER,
+                description = "JWT token для аутентификации"
+        )
+})
 public class SwaggerConfig {
 }
