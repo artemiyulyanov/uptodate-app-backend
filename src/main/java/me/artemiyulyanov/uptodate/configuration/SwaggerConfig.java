cd30f8cb048f41e3a8dcacf002b1b28aecf70f9d
artemiyulyanov <artemiyulyanov.career@gmail.com>
2025-05-12 00:05:38 +0300

    chore: Swagger configuration added

--- src/main/java/me/artemiyulyanov/uptodate/configuration/SwaggerConfig.java
+++ src/main/java/me/artemiyulyanov/uptodate/configuration/SwaggerConfig.java
@@ -3,7 +3,9 @@ package me.artemiyulyanov.uptodate.configuration;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @OpenAPIDefinition(
         info = @Info(title = "Uptodate API", version = "v1.0.0"),
         servers = @Server(url = "https://artemyulyanov.com", description = "Production server")
